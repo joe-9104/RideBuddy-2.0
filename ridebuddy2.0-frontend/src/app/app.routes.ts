7e3d6f74b5d96238dcb81c74ac56a3f9c7ba02dc
IdrisLaabidi <idrislaabidi@gmail.com>
2025-11-13 10:31:13 +0100

    contact page implemented

--- ridebuddy2.0-frontend/src/app/app.routes.ts
+++ ridebuddy2.0-frontend/src/app/app.routes.ts
@@ -8,5 +8,6 @@ export const routes: Routes = [
   { path: 'dashboard', component: Dashboard, canActivate: [AuthGuard] },
   { path: 'login', loadComponent: () => import('./features/auth/login/login').then(m => m.Login)},
   { path: 'register', loadComponent: () => import('./features/auth/register/register').then(m => m.Register)},
+  { path: 'contact' , loadComponent: () => import('./features/contact/contact').then(m => m.Contact)},
   { path: '**', redirectTo: ''},
 ];
