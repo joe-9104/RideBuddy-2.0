c40f21a19d0083bcff820b627742bff591c45322
IdrisLaabidi <idrislaabidi@gmail.com>
2025-11-27 10:55:28 +0100

    dashboard using @for instead ngif

--- ridebuddy2.0-frontend/src/app/features/dashboard/dashboard.ts
+++ ridebuddy2.0-frontend/src/app/features/dashboard/dashboard.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {AuthService} from '../../core/services/auth.service';
-import {AsyncPipe, CommonModule, NgIf} from '@angular/common';
+import {AsyncPipe, CommonModule} from '@angular/common';
 import {BehaviorSubject} from 'rxjs';
 import {User} from '../../app.models';
 import {Navbar} from '../../shared/navbar/navbar';
@@ -8,7 +8,6 @@ import {Navbar} from '../../shared/navbar/navbar';
 @Component({
   selector: 'app-dashboard',
   imports: [
-    NgIf,
     AsyncPipe,
     CommonModule,
     Navbar,
