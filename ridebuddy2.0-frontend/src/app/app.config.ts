3762236c9a8d8aa413ee702f054a973c70ec046d
IdrisLaabidi <idrislaabidi@gmail.com>
2025-12-06 13:34:10 +0100

    edit app.config.ts

--- ridebuddy2.0-frontend/src/app/app.config.ts
+++ ridebuddy2.0-frontend/src/app/app.config.ts
@@ -4,7 +4,6 @@ import {
   provideZoneChangeDetection
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
@@ -12,8 +11,6 @@ import {environment} from '../environment';
 import {provideAuth, getAuth} from '@angular/fire/auth';
 import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 import {getStorage, provideStorage} from '@angular/fire/storage';
-
-// FontAwesome imports for standalone bootstrap
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 
@@ -27,9 +24,7 @@ export const appConfig: ApplicationConfig = {
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
-    // Make FontAwesomeModule available to standalone components
     FontAwesomeModule,
-    // Initialize icon library (adds all solid icons for convenience)
     {
       provide: 'FA_ICON_INIT',
       useFactory: (library: FaIconLibrary) => {
